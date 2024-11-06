6f954f340495644a6265fb7bffde31f80914d23f
Jann Idar Hillestad <jihillestad@hotmail.com>
2024-11-06 02:14:02 +0100

    Anonymized storage_account_name

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name  = "tfstate_rg"
-    storage_account_name = "tfstate30106"
+    storage_account_name = "tfstateNUMBER"
     container_name       = "tfstate"
     key                  = "terraform-shagent.tfstate"
   }
